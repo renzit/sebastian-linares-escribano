6d8be196f30413c1a9bc438227a6c8117fed4355
Renzo <renzo.mayer@vix.com>
2020-05-29 02:23:35 -0300

    update config

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
-  siteLogo: '/icons/icon-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
-  pathPrefix: '', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
-  siteRss: '/rss.xml',
-  siteFBAppID: '', // FB Application ID for using app insights
-  googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
-  themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
-  backgroundColor: '#ffffff', // Used for setting manifest background color.
-}
+  siteTitle: "Escribano Sebastián Linares Mayer", // Site title.
+  siteTitleAlt: "Escribano Publico Uruguay Notario Publico Montevideo Notarial", // Alternative site title for SEO.
+  siteLogo: "/icons/icon-512x512.png", // Logo used for SEO and manifest.
+  siteUrl: "https://esclinaresmayer.netlify.app/", // Domain of your website without pathPrefix.
+  pathPrefix: "", // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
+  siteDescription:
+    "Escribano Publico Uruguay Notario Publico en Montevideo Servicios Notariales en todo el Uruguay: Consulta Confidencial y Gratuita.", // Website description used for RSS feeds/meta description tag.
+  siteRss: "/rss.xml",
+  siteFBAppID: "", // FB Application ID for using app insights
+  googleTagManagerID: "", // GTM tracking ID.
+  disqusShortname: "gatsby-business-starter", // Disqus shortname.
+  userName: "Sebastian Linares",
+  userTwitter: "vaibhaved",
+  userLocation: "Delhi NCR, India",
+  userDescription: "",
+  copyright:
+    "Copyright © Gatsby Starter Business 2018-2019. All Rights Reserved.", // Copyright string for the footer of the website and RSS feed.
+  themeColor: "#00d1b2", // Used for setting manifest and progress theme colors.
+  backgroundColor: "#ffffff" // Used for setting manifest background color.
+};
